129ae9ed8ccc49f173eeb7703b1dbfcce4f7b169
anonymous4y <harshsharma1481@gmail.com>
2023-12-23 18:40:02 +0530

    create teh about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to IMDB-clone, your ultimate online movie database. Immerse
+        yourself in a vast collection of films spanning genres, eras, and
+        cultures. From timeless classics to the latest releases, our
+        comprehensive database provides in-depth information on each movie,
+        including cast details, plot summaries, and critical reviews.
+      </p>
+      <p>
+        Join our passionate community of movie enthusiasts, engage in
+        discussions, and discover personalized recommendations based on your
+        preferences. Stay connected with the cinematic world through our latest
+        news, exclusive interviews, and curated content. IMDB-clone is your
+        go-to destination for a seamless and immersive cinematic experience,
+        where every movie tells a story.
+      </p>
+      <p>
+        This website is built using Next.js 13.5.4, and tailwindCss. Happy
+        Browsing !!
+      </p>
+    </div>
+  );
+}
+
+export default page;
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Providers from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,14 +14,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* Header */}
-        <Header />
+        <Providers>
+          {/* Header */}
+          <Header />
 
-        {/* Navbar */}
+          {/* Navbar */}
 
-        {/* Search */}
+          {/* Search */}
 
-        {children}
+          {children}
+        </Providers>
       </body>
     </html>
   );
